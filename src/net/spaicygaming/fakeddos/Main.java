f82831a70c8406be43ccd55148b5848e76d979e6
Spaicy <spaicygraphic@gmail.com>
2017-05-15 15:34:57 +0200

    v1.1

--- src/net/spaicygaming/fakeddos/Main.java
+++ src/net/spaicygaming/fakeddos/Main.java
@@ -1,17 +1,19 @@
 package net.spaicygaming.fakeddos;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.Server;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import net.md_5.bungee.api.ChatColor;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
@@ -21,7 +23,9 @@ import net.minecraft.server.v1_8_R3.PlayerConnection;
 
 public class Main extends JavaPlugin implements Listener {
 
-	String ver = "1.0";
+	String ver = "1.1";
+	String projectlink= "http://bit.ly/FakeDDoS";
+	String prefix = ChatColor.DARK_GRAY + " [" + ChatColor.AQUA + "Fake" + ChatColor.RED + "DDoS" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET;
 	
 	public void onEnable(){
 		Server server = getServer();
@@ -30,12 +34,12 @@ public class Main extends JavaPlugin implements Listener {
 	    console.sendMessage(ChatColor.GREEN + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 	    console.sendMessage(ChatColor.AQUA + "           Fake" + ChatColor.RED + "DDoS");
 	    console.sendMessage(ChatColor.AQUA + "          Version " + this.ver);
-	 //console.sendMessage(ChatColor.GOLD + " Project:" + ChatColor.RED + ChatColor.ITALIC + "http://bit.ly/");
+	    console.sendMessage(ChatColor.GOLD + " Project:" + ChatColor.RED + ChatColor.ITALIC + projectlink);
 	    console.sendMessage(ChatColor.GREEN + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 	    
 	    Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	    saveDefaultConfig();
-	    if (!getConfig().getString("ConfigVersion").equals("1.0")) {
+	    if (!getConfig().getString("ConfigVersion").equals("1.1")) {
 	        console.sendMessage("[FakeDDoS] " + ChatColor.RED + "OUTDATED CONFIG FILE DETECTED, PLEASE DELETE THE OLD ONE!");
 	    }
 	}	
@@ -54,21 +58,21 @@ public class Main extends JavaPlugin implements Listener {
 				if (sender.hasPermission("fakeddos.use")) {
 			
 					if (args.length == 0) {
-						sender.sendMessage(ChatColor.DARK_GRAY + " [" + ChatColor.AQUA + "Fake" + ChatColor.RED + "DDoS" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET + getConfig().getString("NoArgsMessage"));	
+						sender.sendMessage(prefix + getConfig().getString("NoArgsMessage"));	
 					}		
 					else {
 							Player target = Bukkit.getServer().getPlayer(args[0]);
 							if (target == null) {
-								sender.sendMessage(ChatColor.DARK_GRAY + " [" + ChatColor.AQUA + "Fake" + ChatColor.RED + "DDoS" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET + getConfig().getString("TargetOffline").replaceAll("%TARGET", args[0]));
+								sender.sendMessage(prefix + getConfig().getString("TargetOffline").replaceAll("%TARGET", args[0]));
 							}
 					
 							else {							
 								if (target == sender) {
-									sender.sendMessage(ChatColor.DARK_GRAY + " [" + ChatColor.AQUA + "Fake" + ChatColor.RED + "DDoS" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET + getConfig().getString("AutoDDoSMessage"));
+									sender.sendMessage(prefix + getConfig().getString("AutoDDoSMessage"));
 								}							
 								else{
 									
-									sender.sendMessage(ChatColor.DARK_GRAY + " [" + ChatColor.AQUA + "Fake" + ChatColor.RED + "DDoS" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET + getConfig().getString("SentDDoSMessage").replaceAll("%TARGET", target.getName().toString())); 
+									sender.sendMessage(prefix + getConfig().getString("SentDDoSMessage").replaceAll("%TARGET", target.getName().toString())); 
 									
 									CraftPlayer craftplayer = (CraftPlayer) target;
 									PlayerConnection connection = craftplayer.getHandle().playerConnection;
@@ -80,26 +84,83 @@ public class Main extends JavaPlugin implements Listener {
 									connection.sendPacket(title);
 									connection.sendPacket(subtitle);									
 									
-									if (getConfig().getBoolean("PlaySounds-Target")) {										
+									//target
+									if (getConfig().getBoolean("Effects.Target.Sounds")) {										
 										target.getWorld().playSound(target.getLocation(), Sound.GHAST_SCREAM, 1, 1);										
 									}
+									if (getConfig().getBoolean("Effects.Target.Particles")) {
+										target.getWorld().playEffect(target.getLocation(), Effect.MOBSPAWNER_FLAMES, 1);
+									}
 									
-									if (getConfig().getBoolean("PlaySounds-Sender")) {
-										((CraftPlayer) sender).getWorld().playSound(((CraftPlayer) sender).getLocation(), Sound.BLAZE_HIT, 1, 1);	
+									//sender
+									if (getConfig().getBoolean("Effects.Sender.Sounds")) {
+										((Entity) sender).getWorld().playSound(((Entity) sender).getLocation(), Sound.BLAZE_HIT, 1, 1);	
+									}
+									if (getConfig().getBoolean("Effects.Sender.Particles")) {
+										((Entity) sender).getWorld().playEffect(((Entity) sender).getLocation(), Effect.MOBSPAWNER_FLAMES, 1);
 									}
 								}
 							}
 					}			
 				
 				} else {
-					sender.sendMessage(ChatColor.DARK_GRAY + " [" + ChatColor.AQUA + "Fake" + ChatColor.RED + "DDoS" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET + getConfig().getString("NoPermissionMessage"));
+					sender.sendMessage(prefix + getConfig().getString("NoDDoSPermissions"));
 				}
 				
 			} else {
-				sender.sendMessage(ChatColor.DARK_GRAY + " [" + ChatColor.AQUA + "Fake" + ChatColor.RED + "DDoS" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET + getConfig().getString("OnlyPlayerCmd"));
+				sender.sendMessage(prefix + getConfig().getString("OnlyPlayerCmd"));
 			}
 		}		
-		return false;
-	}
-	
+		
+		if (alias.equalsIgnoreCase("fakeddos")){
+			if (args.length == 0){
+				sender.sendMessage("");
+				sender.sendMessage(ChatColor.RED + "     --=-=" + ChatColor.GOLD  + " FakeDDoS " + ChatColor.GRAY + "v" + ver + ChatColor.RED + " =-=--");
+				sender.sendMessage(ChatColor.AQUA + "   /ddos <player> " + ChatColor.GREEN + "->" + ChatColor.GRAY + " FakeDDoS a player.");
+				sender.sendMessage(ChatColor.AQUA + "   /fakeddos info " + ChatColor.GREEN + "->" + ChatColor.GRAY + " Shows Info.");
+				sender.sendMessage(ChatColor.AQUA + "   /fakeddos reload " + ChatColor.GREEN + "->" + ChatColor.GRAY + " Reloads the Config.");
+				sender.sendMessage(ChatColor.RED + "          --=-=-=-=-=-=--");
+				sender.sendMessage("");
+			}
+			if (args.length == 1){
+				if (args[0].equalsIgnoreCase("reload")){
+					if (sender.hasPermission("fakeddos.reload")){
+						if (sender instanceof Player){
+							reloadConfig();
+							sender.sendMessage(prefix + "§cConfig Reloaded.");
+							getLogger().info("Config Reloaded.");
+						} else {
+							getLogger().info("Config Reloaded.");
+							reloadConfig();
+						}
+					
+					} else {
+						sender.sendMessage(prefix + getConfig().getString("NoReloadPermissions"));
+					}
+				}
+				else if (args[0].equalsIgnoreCase("info")){
+					sender.sendMessage(ChatColor.DARK_GREEN + "    --=-=-=-=-=-=-=-=-=--");
+					sender.sendMessage(ChatColor.AQUA + "         Fake" + ChatColor.RED + "DDoS " + ChatColor.GRAY + "v" + ver);
+					sender.sendMessage(ChatColor.GOLD + "    Project: " + ChatColor.DARK_RED + ChatColor.ITALIC + projectlink);
+					sender.sendMessage(ChatColor.DARK_GREEN + "      --=-=-=-=-=-=-=--");
+					sender.sendMessage("");					
+				}
+				else {
+					if (sender instanceof Player){
+						sender.sendMessage(prefix + getConfig().getString("WrongCommand"));
+					}else{
+						getLogger().info(getConfig().getString("WrongCommand"));
+					}
+				}
+			}
+			if (args.length > 1){
+				if (sender instanceof Player){
+					sender.sendMessage(prefix + getConfig().getString("TooManyArgs"));
+				}else{
+					getLogger().info(getConfig().getString("TooManyArgs"));
+				}
+			}
+		}
+	return false;
+	}	
 }
